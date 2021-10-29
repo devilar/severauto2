ad37103243bade3c46b1fc994faa638ec41b667b
NYasakov <Ohle|Me8>
2021-10-29 18:31:57 +0300

    go

---
+++ src/components/HeaderProfile/HeaderProfile.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import './headerProfile.scss';
+
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import MenuList from "@material-ui/core/MenuList";
+import MenuItem from "@material-ui/core/MenuItem";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Hidden from "@material-ui/core/Hidden";
+import Poppers from "@material-ui/core/Popper";
+// @material-ui/icons
+import FaceIcon from '@mui/icons-material/Face';
+//core components
+import Button from "components/CustomButtons/Button.js";
+
+import styles from "assets/jss/material-dashboard-react/dropdownStyle.js";
+import {Typography} from "@mui/material";
+
+const useStyles = makeStyles(styles);
+
+export default function HeaderProfile() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(null);
+  const handleToggle = event => {
+    if (open && open.contains(event.target)) {
+      setOpen(null);
+    } else {
+      setOpen(event.currentTarget);
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(null);
+  };
+  return (
+      <div>
+        <div className={classes.manager}>
+            <Button
+                color={window.innerWidth > 959 ? "transparent" : "white"}
+                simple={!(window.innerWidth > 959)}
+                aria-owns={open ? "menu-list-grow" : null}
+                aria-haspopup="true"
+                onClick={handleToggle}
+                className={classes.buttonLink}
+            >
+            <FaceIcon style={{fill:"#000000"}}/>
+                <Typography ml='5px' color='#ffffff' align='center' component="h6" variant="p" fontSize={14}>Сергей</Typography>
+            <Hidden mdUp implementation="css">
+              <p onClick={handleClose} className={classes.linkText}>
+                Notification
+              </p>
+            </Hidden>
+          </Button>
+          <Poppers
+              open={Boolean(open)}
+              anchorEl={open}
+              transition
+              disablePortal
+              className={
+                classNames({ [classes.popperClose]: !open }) +
+                " " +
+                classes.pooperNav
+              }
+          >
+            {({ TransitionProps, placement }) => (
+                <Grow
+                    {...TransitionProps}
+                    id="menu-list-grow"
+                    style={{
+                      transformOrigin:
+                          placement === "bottom" ? "center top" : "center bottom"
+                    }}
+                >
+                  <Paper>
+                    <ClickAwayListener onClickAway={handleClose}>
+                      <MenuList role="menu">
+                        <MenuItem
+                            onClick={handleClose}
+                            className={classes.dropdownItem}
+                        >
+                          Mike John responded to your email
+                        </MenuItem>
+                        <MenuItem
+                            onClick={handleClose}
+                            className={classes.dropdownItem}
+                        >
+                          You have 5 new tasks
+                        </MenuItem>
+                        <MenuItem
+                            onClick={handleClose}
+                            className={classes.dropdownItem}
+                        >
+                          test
+                        </MenuItem>
+                        <MenuItem
+                            onClick={handleClose}
+                            className={classes.dropdownItem}
+                        >
+                          test2
+                        </MenuItem>
+                        <MenuItem
+                            onClick={handleClose}
+                            className={classes.dropdownItem}
+                        >
+                          test3
+                        </MenuItem>
+                      </MenuList>
+                    </ClickAwayListener>
+                  </Paper>
+                </Grow>
+            )}
+          </Poppers>
+        </div>
+      </div>
+  );
+}
---
+++ src/components/Lang/lang.js
@@ -0,0 +1,3 @@
+export const tarbarsss = (num) =>{
+  return `Логин не должен быть  меньше чем ${num} символов`
+};
